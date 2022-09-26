65d940ab63a91e6a457b00b63b5e466b941f5652
GGrop <giuliano.gropponi@gmail.com>
2022-09-26 12:45:21 -0300

    a

---
+++ src/App.jsx
@@ -0,0 +1,124 @@
+import { Login } from './components/views/auth/Login/Login';
+import { Register } from './components/views/auth/Register/Register';
+import { Error404 } from './components/views/Error404/Error404';
+import {Tasks } from './components/views/Tasks/Tasks'
+import { Registered } from './components/views/Registered/Registered';
+
+import {Route, Routes, Navigate, useLocation} from 'react-router-dom'
+import { Donate } from './components/views/Donate/Donate';
+import {AnimatePresence, motion} from 'framer-motion'
+
+const RequireAuth=({children})=>{
+    if(!localStorage.getItem('token')){
+        return <Navigate to='/login'replace={true}/>
+    }
+        return children;
+}
+
+const pageTransition={
+    in:{
+        opacity:1,
+    },
+    out:{
+        opacity:0,
+    }
+}
+
+export const App=()=>{
+    const location=useLocation()
+
+    return(
+        <>
+            <AnimatePresence>
+            <Routes location={location} key={location.pathname}>
+            <Route
+                path="/"
+                element={
+                <motion.div
+                    className='page'
+                    initial='out'
+                    animate='in'
+                    exit='out'
+                    variants={pageTransition}
+                >
+                    <RequireAuth>
+                        <Tasks/>
+                    </RequireAuth>
+                </motion.div>
+                }
+            />
+            <Route
+                path="/login"
+                element={
+                <motion.div
+                    className='page'
+                    initial='out'
+                    animate='in'
+                    exit='out'
+                    variants={pageTransition}
+                >
+                    <Login/>
+                </motion.div>
+                }
+            />
+            <Route
+                path="/register"
+                element={
+                <motion.div
+                    className='page'
+                    initial='out'
+                    animate='in'
+                    exit='out'
+                    variants={pageTransition}
+                >
+                    <Register/>
+                </motion.div>
+                }
+            />
+            <Route
+                path="/registered/:teamID"
+                element={
+                <motion.div
+                    className='page'
+                    initial='out'
+                    animate='in'
+                    exit='out'
+                    variants={pageTransition}
+                >
+                    <Registered/>
+                </motion.div>
+                }
+            />
+            <Route
+                path="/donate"
+                element={
+                <motion.div
+                    className='page'
+                    initial='out'
+                    animate='in'
+                    exit='out'
+                    variants={pageTransition}
+                >
+                    <Donate/>
+                </motion.div>
+                }
+            />
+            <Route
+                path="/*"
+                element={
+                <motion.div
+                    className='page'
+                    initial='out'
+                    animate='in'
+                    exit='out'
+                    variants={pageTransition}
+                >
+                    <Error404/>
+                </motion.div>
+                }
+            />
+        </Routes>
+        </AnimatePresence>
+        </>
+    )
+}
